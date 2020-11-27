263d4b4e0e0fea8302a48a33c2769ac8250be311
ari97ag <74934217+ari97ag@users.noreply.github.com>
2020-11-27 03:36:10 -0300

    OPTICS en R listo

--- OPTICS/R/OPTICS2_algoritmo.R
+++ OPTICS/R/OPTICS2_algoritmo.R
@@ -1,59 +1,48 @@
+library("fpc")
+library("gap")
+library("amap")
+library("fastICA")
+library("reshape2")
 library("opticskxi")
-library("dbscan")
 
 setwd("/Users/ari97ag/Documents/GitHub/Universidad/OPTICS/R/")
 base<-read.csv("Mall_Customers.csv", header=TRUE,sep=",", na.strings="NA")
 attach(base)
 base_1 <- base[-c(1:2)] %>% scale
 
-#DBSCAN
+# n_xi: Cantidad de clusters que espero
+# pts: Observaciones minimas dentro del nucleo del cluster
+# dist: Distancias
+optics_parametros_1<-expand.grid(n_xi = 3:6, pts = c(20,30),dist = "euclidean")
+optics_parametros_2<-expand.grid(n_xi = 3:6, pts = c(20,30),dist = "manhattan")
 
-modelDBSCAN1<-dbscan(base_1,eps=0.6, MinPts = 8,borderPoints = TRUE)
-head(cbind(base_1,modeloDBSCAN1$cluster))
-hullplot(base_1,modeloDBSCAN1$cluster, main = "Convex cluster Hulls, eps= 0.4")
+modelos_optics1 <- opticskxi_pipeline(base_1, optics_parametros_1, n_cores = 1)
+ggplot_kxi_metrics(modelos_optics1, n = 8)
 
-#OPTICS
-opticskxi_pipeline(base_1, 
-                   df_params = expand.grid(n_xi = 1:10, pts =c(20, 30, 40), 
-                                           dist = c("euclidean", "abscorrelation"), 
-                                           dim_red =c("identity", "PCA", "ICA"), 
-                                           n_dimred_comp = c(5, 10, 20)),
-                   n_cores = 1)
+modelos_optics2 <-opticskxi_pipeline(base_1, optics_parametros_2, n_cores = 1)
+ggplot_kxi_metrics(modelos_optics2, n = 8)
 
+#Grafica de alcanzabilidad de los 4 mejores de los modelos con distancia de manhattan
+gtable_kxi_profiles(modelos_optics1) %>% plot
 
+#Seleccion del mejor modelo optics
+mejor_modelo_optics <- get_best_kxi(modelos_optics1, rank = 1)
+clusters <- mejor_modelo_optics$clusters;clusters
 
-modelOPTICS<-optics(base_1, eps=10, minPts = 10)
+#Clustering por sexo
+qq<-rep(1,200)
+base_2<-cbind(base_1,clusters,qq)
+aggregate(qq, by=list(clusters,Gender), sum)
 
-kk<-extractXi(modelOPTICS,xi=0.05)
-kk$clusters_xi
-hullplot(base_1,kk)
+fortify_pca(base_1, sup_vars = data.frame(Clusters = clusters)) %>% 
+  ggpairs('Clusters', ellipses = TRUE)
 
-dend <- as.dendrogram(kk);dend
-plot(dend, ylab = "Reachability dist.", leaflab = "none")
+x_referencia
+x_comparativo
 
-
-library("fpc")
-library("gap")
-library("amap")
-library("fastICA")
-library("reshape2")
-qq<-data('hla')
-m_hla <- hla[-c(1:2)] %>% scale
-df_params_hla <- expand.grid(n_xi = 3:5, pts = c(20, 30),
-                             dist = c('manhattan', 'euclidean'))
-df_kxi_hla <- opticskxi_pipeline(m_hla, df_params_hla)
-ggplot_kxi_metrics(df_kxi_hla, n = 8)
-gtable_kxi_profiles(df_kxi_hla) %>% plot
-best_kxi_hla <- get_best_kxi(df_kxi_hla, rank = 2)
-clusters_hla <- best_kxi_hla$clusters
-
-fortify_pca(m_hla, sup_vars = data.frame(Clusters = clusters_hla)) %>%
-  ggpairs('Clusters', ellipses = TRUE, variables = TRUE)
-
-#Pipeline me da la distancia de las metricas
-
-#Para agregar y categorizar la base
-kk<-rep(1,200)
-hla2<-cbind(hla,clusters_hla,kk)
-aggregate(kk, by=list(hla2$clusters_hla), sum)
+# Para realizar una validacion externa podriamos hacerlo si tenemos los 
+# datos categorizados originales de la base o por medio de la comparacion con otro metodo o modelo
+library("fossil")
+#indice de Rand
+rand.index(x_referencia, x_comparativo)
 
