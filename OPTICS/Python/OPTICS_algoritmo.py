bf34fd60a56d22c0a1594b863ab7528ace34caea
ari97ag <74934217+ari97ag@users.noreply.github.com>
2020-12-01 03:34:36 -0300

    Best algoritmo

--- OPTICS/Python/OPTICS_algoritmo.py
+++ OPTICS/Python/OPTICS_algoritmo.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 from matplotlib import gridspec
 from sklearn.cluster import OPTICS, cluster_optics_dbscan
 from sklearn.preprocessing import normalize, StandardScaler
+from sklearn import metrics
 
 #Carga de la data
 X = pd.read_csv('Mall_Customers.csv',sep=';')
@@ -27,50 +28,54 @@ X_escalado = pd.DataFrame(X_escalado)
 X_escalado.columns = X.columns
 X_escalado.head()
 
-# Construcción del modelo de clasificación OPTICS, diferentes metricas
+# Vector con posiciones del 0 al 199
+vector = np.arange(len(X_escalado))
+print(vector)
 
+# Construcción del modelo de clasificación OPTICS, diferentes metricas
 # min_samples: minimo de observaciones en el nucleo de los clusters
 # metric: medida de distancia, si no se especifica ocupa << minkowski >>
 # xi: La inclinación mínima en el valle, para que sea efectivamnete considerado como un cluster
 # min_cluster_size: Número mínimo de observaciones en un cluster. Si no hay ninguna, se utiliza el valor de min_samples.
 # Las ultimas dos solo se pueden utilizar cuando cluster_method='xi'.
 
+# Caso distancia manhattan
 modelo_optics1 = OPTICS(min_samples=10, metric='manhattan',xi=0.05,
-                        min_cluster_size=0.05, cluster_method='xi')
-modelo_optics2 = OPTICS(min_samples=10, metric='euclidean',xi=0.05,
                         min_cluster_size=0.05, cluster_method='xi')
 
 # Insertando la base en los modelos
 modelo_optics1.fit(X_escalado)
-modelo_optics2.fit(X_escalado)
 
 # Clusters según DBSCAN con epsilon = 0.5
-DBSCAN1 = cluster_optics_dbscan(reachability = modelo_optics1.reachability_,
+DBSCAN11 = cluster_optics_dbscan(reachability = modelo_optics1.reachability_,
                                 core_distances = modelo_optics1.core_distances_,
                                 ordering = modelo_optics1.ordering_, eps = 0.5)
-print(DBSCAN1)
+print(DBSCAN11)
 
 # Clusters según DBSCAN con epsilon = 2.0
-DBSCAN2 = cluster_optics_dbscan(reachability = modelo_optics1.reachability_,
+DBSCAN12 = cluster_optics_dbscan(reachability = modelo_optics1.reachability_,
                                 core_distances = modelo_optics1.core_distances_,
                                 ordering = modelo_optics1.ordering_, eps = 2)
-print(DBSCAN2)
+print(DBSCAN12)
 
-# Vector con posiciones del 0 al 199
-vector = np.arange(len(X_escalado))
-print(vector)
+# Distancia de accesibilidad en cada punto observado
+reachability1 = modelo_optics1.reachability_[modelo_optics1.ordering_]
+print(reachability1)
 
-# Distancia de alcance en cada punto observado
-reachability = modelo_optics1.reachability_[modelo_optics1.ordering_]
-print(reachability)
+# Clasificación
+clusters1 = modelo_optics1.labels_[modelo_optics1.ordering_]
+print(clusters1)
 
-# Observaciones que logran ser clasificadas con exito y aquellas que no
-clusters = modelo_optics1.labels_[modelo_optics1.ordering_]
-print(clusters)
-
-from sklearn.metrics import silhouette_score
-silo=silhouette_score(X, clusters)
-print('El valor de la silueta es:',silo)
+# Validacion interna
+print("Coeficiente Silhouette: %0.3f"
+      % metrics.silhouette_score(X_escalado, clusters1))
+# Los clusters estan muy cerca al hecho de estar uno sobre otro
+print("Coeficiente Davies-Bouldin: %0.3f"
+      % metrics.davies_bouldin_score(X_escalado, clusters1))
+# Validacion externa
+# La separacion entre los cluster no es muy buena pues tienen un comportameinto jerarquico
+print("Coeficiente ARI: %0.3f"
+      % metrics.cluster.adjusted_rand_score(clusters1, DBSCAN11))
 
 # Definiendo la ventana de visualizacion
 plt.figure(figsize=(10, 7))
@@ -83,14 +88,14 @@ ax4 = plt.subplot(G[1, 2])
 # Grafica distancia de alcance
 colors = ['c.', 'b.', 'r.', 'y.', 'g.']
 for Class, colour in zip(range(0, 5), colors):
-    Xk = vector[clusters == Class]
-    Rk = reachability[clusters == Class]
+    Xk = vector[clusters1 == Class]
+    Rk = reachability1[clusters1 == Class]
     ax1.plot(Xk, Rk, colour, alpha=0.3)
-ax1.plot(vector[clusters == -1], reachability[clusters == -1], 'k.', alpha=0.3)
+ax1.plot(vector[clusters1 == -1], reachability1[clusters1 == -1], 'k.', alpha=0.3)
 ax1.plot(vector, np.full_like(vector, 2., dtype=float), 'k-', alpha=0.5)
 ax1.plot(vector, np.full_like(vector, 0.5, dtype=float), 'k-.', alpha=0.5)
-ax1.set_ylabel('Distancia $\epsilon$')
-ax1.set_title('Gráfica de Alcance')
+ax1.set_ylabel('Puntaje de Accecibilidad')
+ax1.set_title('Gráfica de Accesibilidad')
 
 # OPTICS Clustering
 colors = ['c.', 'b.', 'r.', 'y.', 'g.']
@@ -106,24 +111,122 @@ ax2.set_title('OPTICS 1 Clustering')
 # DBSCAN Clustering con epsilon = 0.5
 colors = ['c', 'b', 'r', 'y', 'g', 'greenyellow']
 for Class, colour in zip(range(0, 6), colors):
-    Xk = X_escalado[DBSCAN1 == Class]
+    Xk = X_escalado[DBSCAN11 == Class]
     ax3.plot(Xk.iloc[:, 0], Xk.iloc[:, 1], colour, alpha=0.3, marker='.')
 
-ax3.plot(X_escalado.iloc[DBSCAN1 == -1, 0],
-         X_escalado.iloc[DBSCAN1 == -1, 1],
+ax3.plot(X_escalado.iloc[DBSCAN11 == -1, 0],
+         X_escalado.iloc[DBSCAN11 == -1, 1],
          'k+', alpha=0.1)
 ax3.set_title('DBSCAN Clustering $\epsilon = 0,5$')
 
 # DBSCAN Clustering con epsilon = 2.0
 colors = ['c.', 'y.', 'm.', 'g.']
 for Class, colour in zip(range(0, 4), colors):
-    Xk = X_escalado.iloc[DBSCAN2 == Class]
+    Xk = X_escalado.iloc[DBSCAN12 == Class]
     ax4.plot(Xk.iloc[:, 0], Xk.iloc[:, 1], colour, alpha=0.3)
 
-ax4.plot(X_escalado.iloc[DBSCAN2 == -1, 0],
-         X_escalado.iloc[DBSCAN2 == -1, 1],
+ax4.plot(X_escalado.iloc[DBSCAN12 == -1, 0],
+         X_escalado.iloc[DBSCAN12 == -1, 1],
          'k+', alpha=0.1)
 ax4.set_title('DBSCAN Clustering $\epsilon = 2,0$')
 
 plt.tight_layout()
 plt.show()
+
+###############################
+# Caso distancia euclidiana
+modelo_optics2 = OPTICS(min_samples=10, metric='euclidean',xi=0.05,
+                        min_cluster_size=0.05, cluster_method='xi')
+
+# Insertando la base en los modelos
+modelo_optics2.fit(X_escalado)
+
+# Clusters según DBSCAN con epsilon = 0.5
+DBSCAN21 = cluster_optics_dbscan(reachability = modelo_optics2.reachability_,
+                                core_distances = modelo_optics2.core_distances_,
+                                ordering = modelo_optics2.ordering_, eps = 0.5)
+print(DBSCAN21)
+
+# Clusters según DBSCAN con epsilon = 2.0
+DBSCAN22 = cluster_optics_dbscan(reachability = modelo_optics2.reachability_,
+                                core_distances = modelo_optics2.core_distances_,
+                                ordering = modelo_optics2.ordering_, eps = 2)
+print(DBSCAN22)
+
+# Distancia de accesibilidad en cada punto observado
+reachability2 = modelo_optics2.reachability_[modelo_optics2.ordering_]
+print(reachability2)
+
+# Clasificación
+clusters2 = modelo_optics2.labels_[modelo_optics2.ordering_]
+print(clusters2)
+
+# Validacion interna
+print("Coeficiente Silhouette: %0.3f"
+      % metrics.silhouette_score(X_escalado, clusters2))
+# Los clusters estan muy cerca al hecho de estar uno sobre otro
+print("Coeficinete Davies-Bouldin: %0.3f"
+      % metrics.davies_bouldin_score(X_escalado, clusters2))
+# La separacion entre los cluster no es muy buena pues tienen un comportameinto jerarquico
+
+# Definiendo la ventana de visualizacion
+plt.figure(figsize=(10, 7))
+G = gridspec.GridSpec(2, 3)
+ax1 = plt.subplot(G[0, :])
+ax2 = plt.subplot(G[1, 0])
+ax3 = plt.subplot(G[1, 1])
+ax4 = plt.subplot(G[1, 2])
+
+# Grafica distancia de alcance
+colors = ['c.', 'b.', 'r.', 'y.', 'g.']
+for Class, colour in zip(range(0, 5), colors):
+    Xk = vector[clusters2 == Class]
+    Rk = reachability2[clusters2 == Class]
+    ax1.plot(Xk, Rk, colour, alpha=0.3)
+ax1.plot(vector[clusters2 == -1], reachability2[clusters2 == -1], 'k.', alpha=0.3)
+ax1.plot(vector, np.full_like(vector, 2., dtype=float), 'k-', alpha=0.5)
+ax1.plot(vector, np.full_like(vector, 0.5, dtype=float), 'k-.', alpha=0.5)
+ax1.set_ylabel('Puntaje de Accecibilidad')
+ax1.set_title('Gráfica de Accesibilidad')
+
+# OPTICS Clustering
+colors = ['c.', 'b.', 'r.', 'y.', 'g.']
+for Class, colour in zip(range(0, 5), colors):
+    Xk = X_escalado[modelo_optics2.labels_ == Class]
+    ax2.plot(Xk.iloc[:, 0], Xk.iloc[:, 1], colour, alpha=0.3)
+
+ax2.plot(X_escalado.iloc[modelo_optics2.labels_ == -1, 0],
+         X_escalado.iloc[modelo_optics2.labels_ == -1, 1],
+         'k+', alpha=0.1)
+ax2.set_title('OPTICS 2 Clustering')
+
+# DBSCAN Clustering con epsilon = 0.5
+colors = ['c', 'b', 'r', 'y', 'g', 'greenyellow']
+for Class, colour in zip(range(0, 6), colors):
+    Xk = X_escalado[DBSCAN21 == Class]
+    ax3.plot(Xk.iloc[:, 0], Xk.iloc[:, 1], colour, alpha=0.3, marker='.')
+
+ax3.plot(X_escalado.iloc[DBSCAN21 == -1, 0],
+         X_escalado.iloc[DBSCAN21 == -1, 1],
+         'k+', alpha=0.1)
+ax3.set_title('DBSCAN Clustering $\epsilon = 0,5$')
+
+# DBSCAN Clustering con epsilon = 2.0
+colors = ['c.', 'y.', 'm.', 'g.']
+for Class, colour in zip(range(0, 4), colors):
+    Xk = X_escalado.iloc[DBSCAN22 == Class]
+    ax4.plot(Xk.iloc[:, 0], Xk.iloc[:, 1], colour, alpha=0.3)
+
+ax4.plot(X_escalado.iloc[DBSCAN22 == -1, 0],
+         X_escalado.iloc[DBSCAN22 == -1, 1],
+         'k+', alpha=0.1)
+ax4.set_title('DBSCAN Clustering $\epsilon = 2,0$')
+
+plt.tight_layout()
+plt.show()
+
+# Resumen de la data clasificada
+final=pd.read_csv('Mall_Customers.csv',sep=';')
+final['Cluster'] = clusters1
+grouped_data = final.groupby('Cluster')
+grouped_data.mean()
--- OPTICS/R/OPTICS2_algoritmo.R
+++ OPTICS/R/OPTICS2_algoritmo.R
@@ -1,52 +1,92 @@
 library("opticskxi")
+library("dbscan")
 library("dplyr")
+library("clusterSim")
+library("fossil")
+library("MixGHD")
+library("fpc")
 
 setwd("/Users/ari97ag/Documents/GitHub/Universidad/OPTICS/R/")
 base<-read.csv("Mall_Customers.csv", header=TRUE,sep=";", na.strings="NA")
 attach(base)
-base_1 <- base[-c(1:2)] %>% scale
+base_1 <- data.frame(base[-c(1:2)] %>% scale)
 
+#minPts optimo
+#k: numero de dimension de la base + 1 seria el minPts.
+kNNdistplot(base_1, k = 4)
+abline(h=.55, col = "red", lty=2)
+
+# DBSCAN
+dbscan_1 <- dbscan(base_1, MinPts = 4, eps = 0.55)
+gg_dbscan_1 <- cbind(base_1, Clusters = dbscan_1$cluster) %>%
+  ggpairs( group = "Clusters", ellipses = T)
+gg_dbscan_1
+
+# OPTICS
 # n_xi: Cantidad de clusters que espero
 # pts: Observaciones minimas dentro del nucleo del cluster
 # dist: Distancias
 
 #Distancia Euclidiana
-optics_parametros_1<-expand.grid(n_xi = 3:5, pts = c(10,30),dist = "euclidean")
+optics_parametros_1<-expand.grid(n_xi = 3:6,pts = c(4,6,8,10),dist = "euclidean")
 modelos_optics1 <- opticskxi_pipeline(base_1, optics_parametros_1)
-ggplot_kxi_metrics(modelos_optics1,n = 6)
+ggplot_kxi_metrics(modelos_optics1,n = 12)
 
 #Grafica de alcanzabilidad de los 4 mejores de los modelos
 gtable_kxi_profiles(modelos_optics1, rank = 1:4) %>% plot
 
 #Distancia Manhattan
-optics_parametros_2<-expand.grid(n_xi = 3:5, pts = c(10,30),dist = "manhattan")
+optics_parametros_2<-expand.grid(n_xi = 3:6, pts = c(4,6,8,10),dist = "manhattan")
 modelos_optics2 <-opticskxi_pipeline(base_1, optics_parametros_2)
-ggplot_kxi_metrics(modelos_optics2, n = 6)
+ggplot_kxi_metrics(modelos_optics2, n = 12)
 
-#Grafica de alcanzabilidad de los 4 mejores de los modelos
+#Grafica de accesibilidad de los 4 mejores modelos
+gtable_kxi_profiles(modelos_optics1, rank = 1:4) %>% plot
 gtable_kxi_profiles(modelos_optics2, rank = 1:4) %>% plot
 
 #Seleccion del mejor modelo optics
-mejor_modelo_optics1 <- get_best_kxi(modelos_optics1, rank = 1)
-mejor_modelo_optics2 <- get_best_kxi(modelos_optics2, rank = 1)
-clusters1 <- mejor_modelo_optics1$clusters;clusters
-clusters2 <- mejor_modelo_optics2$clusters;clusters
+mejor_modelo_optics1 <- get_best_kxi(modelos_optics1, rank = 1);mejor_modelo_optics1 
+mejor_modelo_optics2 <- get_best_kxi(modelos_optics1, rank = 2);mejor_modelo_optics2
+mejor_modelo_optics3 <- get_best_kxi(modelos_optics2, rank = 1);mejor_modelo_optics3
+mejor_modelo_optics4 <- get_best_kxi(modelos_optics2, rank = 2);mejor_modelo_optics4
 
-#Clustering OPTICS 1
-base_2<-cbind(base,clusters)
-kk<-base_2[order(base_2$clusters),]
+clusters1 <- mejor_modelo_optics1$clusters;clusters1
+clusters2 <- mejor_modelo_optics2$clusters;clusters2
+clusters3 <- mejor_modelo_optics3$clusters;clusters3
+clusters4 <- mejor_modelo_optics4$clusters;clusters4
+
+clusters1[1]=0 #NA
+clusters2[1]=0 #NA
+clusters3[1]=0 #NA
+clusters4[1]=0 #NA
+
+# Coeficiente de Davies-Bouldin
+print(index.DB(base_1, clusters1, centrotypes="centroids",p=2))
+print(index.DB(base_1, clusters2, centrotypes="centroids",p=2))
+print(index.DB(base_1, clusters3, centrotypes="centroids",p=1))
+print(index.DB(base_1, clusters4, centrotypes="centroids",p=1))
+
+# Clustering OPTICS 4
+base_2<-cbind(base,clusters4)
+kk<-base_2[order(base_2$clusters4),]
 base_3 <- base_2[-c(1:2)]
-grupos<-aggregate(. ~ clusters, base_3, mean);grupos
+grupos<-aggregate(. ~ clusters4, base_3, mean);grupos
 
-fortify_pca(base_1, sup_vars = data.frame(Clusters = clusters1)) %>% 
+fortify_pca(base_1, sup_vars = data.frame(Clusters = clusters4)) %>% 
   ggpairs('Clusters', ellipses = T)
 
-#VALIDACION EXTERNA
-x_referencia
-x_comparativo
+# VALIDACION EXTERNA
+x_referencia<-dbscan_1$cluster
+x_comparativo<-clusters4
 
 # Para realizar una validacion externa podriamos hacerlo si tenemos los 
 # datos categorizados originales de la base o por medio de la comparacion con otro metodo o modelo
-library("fossil")
-#indice de Rand
+# Indice de Rand
 rand.index(x_referencia, x_comparativo)
+# Indice de Rand Ajustado
+ARI(x=x_referencia, y=x_comparativo)
+
+# Más metricas comparativas
+clust_stats <- cluster.stats(d = dist(base_1), x_referencia, x_comparativo)
+clust_stats$corrected.rand
+clust_stats$vi
